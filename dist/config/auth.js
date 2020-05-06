25ae013051454c8700f9e41b824fa8f17ff7a4aa
luizmaciel <luizmaciel@denox.com.br>
2020-05-05 21:02:00 -0300

    buikl

---
+++ dist/config/auth.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = {
+    jwt: {
+        secret: '61378cf1bf557b8e7d69e05b2126614d',
+        expiresIn: '1d',
+    },
+};
